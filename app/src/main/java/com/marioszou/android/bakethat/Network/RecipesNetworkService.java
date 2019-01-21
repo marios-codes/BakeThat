920866e51685c18f585b4d37fac51b1d2e6b2c41
msrowley <msrowley@gmail.com>
2019-01-21 18:59:22 +0200

    Implement Retrofit response models

--- app/src/main/java/com/marioszou/android/bakethat/Network/RecipesNetworkService.java
+++ app/src/main/java/com/marioszou/android/bakethat/Network/RecipesNetworkService.java
@@ -1,5 +1,12 @@
 package com.marioszou.android.bakethat.Network;
 
+import com.marioszou.android.bakethat.Models.Recipe;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface RecipesNetworkService {
 
+  @GET("/topher/2017/May/59121517_baking/baking.json")
+  Call<List<Recipe>> getRecipes();
 }
