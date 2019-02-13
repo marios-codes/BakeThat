d6da4c0f4d83351b51d3a24d696a076df8429aad
msrowley <msrowley@gmail.com>
2019-02-13 14:56:40 +0200

    Show fetched recipes in SelectRecipeActivity

---
+++ app/src/main/java/com/marioszou/android/bakethat/Adapters/RecipesAdapter.java
@@ -0,0 +1,117 @@
+package com.marioszou.android.bakethat.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.marioszou.android.bakethat.Adapters.RecipesAdapter.RecipesAdapterViewHolder;
+import com.marioszou.android.bakethat.Models.Recipe;
+import com.marioszou.android.bakethat.R;
+import java.util.ArrayList;
+import java.util.List;
+import timber.log.Timber;
+
+public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapterViewHolder> {
+
+
+  /**
+   * The interface that receives onClick messages.
+   */
+  public interface RecipesAdapterOnClickHandler {
+
+    void onClick(Recipe recipe);
+  }
+
+  /*
+   * An on-click handler to make it easy for an Activity to interface with the RecyclerView
+   */
+  private final RecipesAdapterOnClickHandler mClickHandler;
+  private List<Recipe> mRecipeList = new ArrayList<>();
+
+  /**
+   * Creates a MoviesAdapter.
+   *
+   * @param clickHandler The on-click handler for this adapter. This single handler is called when
+   * an item is clicked.
+   */
+  public RecipesAdapter(RecipesAdapterOnClickHandler clickHandler) {
+    this.mClickHandler = clickHandler;
+  }
+
+  @NonNull
+  @Override
+  public RecipesAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+    View view = inflater.inflate(R.layout.item_recipe_cardview, viewGroup, false);
+
+    return new RecipesAdapterViewHolder(view);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull RecipesAdapterViewHolder viewHolder, int position) {
+    String recipeName = mRecipeList.get(position).getName();
+    viewHolder.recipeNameTextView.setText(recipeName);
+
+    int imageResourceId = 0;
+    switch (recipeName) {
+      case "Nutella Pie":
+        imageResourceId = R.drawable.nutella_pie;
+        break;
+      case "Brownies":
+        imageResourceId = R.drawable.brownies;
+        break;
+      case "Yellow Cake":
+        imageResourceId = R.drawable.yellow_cake;
+        break;
+      case "Cheesecake":
+        imageResourceId = R.drawable.cheesecake;
+        break;
+      default:
+        Timber.e("No image resource found for given imageResourceId");
+        break;
+    }
+    if (imageResourceId != 0) {
+      viewHolder.recipeImageView.setImageResource(imageResourceId);
+    }
+  }
+
+  @Override
+  public int getItemCount() {
+    if (null == mRecipeList) {
+      return 0;
+    }
+    return mRecipeList.size();
+  }
+
+  public void setRecipeList(List<Recipe> recipeList) {
+    mRecipeList = recipeList;
+    notifyDataSetChanged();
+  }
+
+  public class RecipesAdapterViewHolder extends RecyclerView.ViewHolder implements OnClickListener {
+
+    @BindView(R.id.tv_card_recipe_name)
+    TextView recipeNameTextView;
+    @BindView(R.id.iv_card_recipe_photo)
+    ImageView recipeImageView;
+
+    public RecipesAdapterViewHolder(View view) {
+      super(view);
+      ButterKnife.bind(this, view);
+      view.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+      Recipe recipe = mRecipeList.get(getAdapterPosition());
+      mClickHandler.onClick(recipe);
+    }
+  }
+
+}
--- app/src/main/java/com/marioszou/android/bakethat/SelectRecipeActivity.java
+++ app/src/main/java/com/marioszou/android/bakethat/SelectRecipeActivity.java
@@ -2,6 +2,14 @@ package com.marioszou.android.bakethat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.marioszou.android.bakethat.Adapters.RecipesAdapter;
+import com.marioszou.android.bakethat.Adapters.RecipesAdapter.RecipesAdapterOnClickHandler;
 import com.marioszou.android.bakethat.Models.Recipe;
 import com.marioszou.android.bakethat.Network.RecipesNetworkService;
 import java.util.List;
@@ -12,13 +20,36 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import timber.log.Timber;
 
-public class SelectRecipeActivity extends AppCompatActivity {
+public class SelectRecipeActivity extends AppCompatActivity implements
+    RecipesAdapterOnClickHandler {
+
+  @BindView(R.id.recyclerview_recipes)
+  RecyclerView mRecyclerView;
+  @BindView(R.id.pb_loading_indicator)
+  ProgressBar mLoadingIndicator;
+
+  private RecipesAdapter mAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_select_recipe);
 
+    ButterKnife.bind(this);
+
+    initViews();
+    fetchRecipesFromApi();
+  }
+
+  private void initViews() {
+    LinearLayoutManager layoutManager = new LinearLayoutManager(SelectRecipeActivity.this);
+    mRecyclerView.setLayoutManager(layoutManager);
+    mRecyclerView.setHasFixedSize(true);
+    mAdapter = new RecipesAdapter(this);
+    mRecyclerView.setAdapter(mAdapter);
+  }
+
+  private void fetchRecipesFromApi() {
     Retrofit retrofit = new Retrofit.Builder()
         .baseUrl("https://d17h27t6h515a5.cloudfront.net")
         .addConverterFactory(GsonConverterFactory.create())
@@ -32,16 +63,32 @@ public class SelectRecipeActivity extends AppCompatActivity {
       @Override
       public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
         Timber.d("Retrofit Response is successful: %s", response.isSuccessful());
+        //hide loading indicator
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
         if (response.isSuccessful()) {
           List<Recipe> recipesList = response.body();
           Timber.d("Number of objects in Recipe List: %s", recipesList.size());
+          mAdapter.setRecipeList(recipesList);
         }
       }
 
       @Override
       public void onFailure(Call<List<Recipe>> call, Throwable t) {
+        //hide loading indicator
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
         Timber.e(t);
       }
     });
+  }
+
+  /**
+   * This method is overridden by our SelectRecipe class in order to handle RecyclerView item
+   * clicks.
+   *
+   * @param recipe The recipe that was clicked
+   */
+  @Override
+  public void onClick(Recipe recipe) {
+
   }
 }
