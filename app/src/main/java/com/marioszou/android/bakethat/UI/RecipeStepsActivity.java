2da075f77069b8f598eec9a098d467fc7a919fc7
msrowley <msrowley@gmail.com>
2019-02-16 16:48:44 +0200

    Show Recipe ingredients in RecipeStepsFragment

--- app/src/main/java/com/marioszou/android/bakethat/UI/RecipeStepsActivity.java
+++ app/src/main/java/com/marioszou/android/bakethat/UI/RecipeStepsActivity.java
@@ -1,15 +1,11 @@
 package com.marioszou.android.bakethat.UI;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import com.marioszou.android.bakethat.Models.Ingredient;
-import com.marioszou.android.bakethat.Models.Recipe;
 import com.marioszou.android.bakethat.R;
-import com.marioszou.android.bakethat.Utils.Ingredients;
-import java.util.List;
-import timber.log.Timber;
 
-public class RecipeStepsActivity extends AppCompatActivity {
+public class RecipeStepsActivity extends AppCompatActivity implements RecipeStepsFragment.OnRecipeStepsFragmentItemClickListener{
 
   public static final String EXTRAS_RECIPE_ITEM = "recipe-item";
 
@@ -18,15 +14,14 @@ public class RecipeStepsActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_recipe_steps);
 
-    Bundle intentExtras = getIntent().getExtras();
-    if (intentExtras != null && intentExtras.get(EXTRAS_RECIPE_ITEM) != null) {
-      Recipe recipe = intentExtras.getParcelable(EXTRAS_RECIPE_ITEM);
-      assert recipe != null;
-      List<Ingredient> ingredientsList = recipe.getIngredients();
-      Timber.d("Ingredients: %s", Ingredients.formatAllIngredientsToString(ingredientsList));
-    } else {
-      Timber.e("Recipe from intent extras is null");
-      finish();
-    }
+  }
+
+  /*
+  This is where RecipeStepsFragment communicates with RecipeStepsActivity and gives this activity
+  a way to communicate with StepDetailsFragment
+   */
+  @Override
+  public void onRecipeStepClick(Uri uri) {
+    //TODO Implement logic
   }
 }
---
+++ app/src/main/java/com/marioszou/android/bakethat/UI/RecipeStepsFragment.java
@@ -0,0 +1,97 @@
+package com.marioszou.android.bakethat.UI;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.marioszou.android.bakethat.Models.Ingredient;
+import com.marioszou.android.bakethat.Models.Recipe;
+import com.marioszou.android.bakethat.R;
+import com.marioszou.android.bakethat.Utils.Ingredients;
+import java.util.List;
+import timber.log.Timber;
+
+/**
+ * A simple {@link Fragment} subclass. Activities that contain this fragment must implement the
+ * {@link OnRecipeStepsFragmentItemClickListener} interface to handle interaction
+ * events.
+ */
+public class RecipeStepsFragment extends Fragment {
+
+  private OnRecipeStepsFragmentItemClickListener mListener;
+
+  @BindView(R.id.tv_ingredients)
+  TextView ingredientsTextView;
+
+  public RecipeStepsFragment() {
+    // Required empty public constructor
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    View view = inflater.inflate(R.layout.fragment_recipe_steps, container, false);
+    //View binding
+    ButterKnife.bind(this, view);
+
+    Bundle intentExtras = getActivity().getIntent().getExtras();
+    if (intentExtras != null && intentExtras.get(RecipeStepsActivity.EXTRAS_RECIPE_ITEM) != null) {
+      Recipe recipe = intentExtras.getParcelable(RecipeStepsActivity.EXTRAS_RECIPE_ITEM);
+      assert recipe != null;
+      List<Ingredient> ingredientsList = recipe.getIngredients();
+      Timber.d("Ingredients: %s", Ingredients.formatAllIngredientsToString(ingredientsList));
+      ingredientsTextView.setText(Ingredients.formatAllIngredientsToString(ingredientsList));
+    } else {
+      Timber.e("Recipe from intent extras is null");
+      getActivity().finish();
+    }
+
+    return view;
+  }
+
+  // TODO: Rename method, update argument and hook method into UI event
+  public void onStepPressed(Uri uri) {
+    if (mListener != null) {
+      mListener.onRecipeStepClick(uri);
+    }
+  }
+
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+    if (context instanceof OnRecipeStepsFragmentItemClickListener) {
+      mListener = (OnRecipeStepsFragmentItemClickListener) context;
+    } else {
+      throw new RuntimeException(context.toString()
+          + " must implement OnRecipeStepsFragmentItemClickListener");
+    }
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    mListener = null;
+  }
+
+  /**
+   * This interface must be implemented by activities that contain this fragment to allow an
+   * interaction in this fragment to be communicated to the activity and potentially other fragments
+   * contained in that activity.
+   * <p>
+   * See the Android Training lesson <a href= "http://developer.android.com/training/basics/fragments/communicating.html"
+   * >Communicating with Other Fragments</a> for more information.
+   */
+  public interface OnRecipeStepsFragmentItemClickListener {
+
+    // TODO: Update argument type and name
+    void onRecipeStepClick(Uri uri);
+  }
+}
